9fc1f7f394d7dcd76ecba36ee17221978ee4947b
Jason McInerney <jason.j.mcn@gmail.com>
2023-02-08 21:51:02 -0800

    Update gitignore

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ Upgraded playwright to the latest version and installed it
 
 ## Github actions for this project
 
+
+
+## Pytest watch mode
+One of my favorite things about automated test is in watch mode
+
+https://johnfraney.ca/blog/pytest-watched-failed-tests/
+
+The fastest way to focus on the testing that needs to be written
+
+```
+ptw -- --last-failed --new-first
+```
+
+Where the -- separator denotes where pytest-watch args stop and pytest arguments start.
+
--- tests/test_site_title.py
+++ tests/test_site_title.py
@@ -1,6 +1,21 @@
 import re
 from playwright.sync_api import Page, expect
 
-def test_home_page_has_title(page: Page):
-    page.goto('https://automationintesting.online')
-    expect(page).to_have_title(re.compile("Restful-booker-platform demo"))
+
+def test_homepage_has_Playwright_in_title(page: Page):
+    page.goto("https://playwright.dev/")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Playwright"))
+
+    # create a locator
+    get_started = page.get_by_role("link", name="Get started")
+
+    # Expect an attribute "to be strictly equal" to the value.
+    expect(get_started).to_have_attribute("href", "/docs/intro")
+
+    # Click the get started link.
+    get_started.click()
+
+    # Expects the URL to contain intro.
+    expect(page).to_have_url(re.compile(".*intro"))
