f2a8f03b4040d5a161e04b585648cea6df3e62c5
Jason McInerney <jason.j.mcn@gmail.com>
2023-02-16 13:39:00 -0800

    Add tests for chapter 2

---
+++ test_repository.py
@@ -0,0 +1,40 @@
+import model
+#import repository
+
+
+def test_repos_1(session):
+    batch = model.Batch("batch1", "RUSTY-SOAPDISH", 100, eta=None)
+
+    repo = repository.SqlAlchemyRepository(session)
+    repo.add(batch)
+    session.commit()
+
+    rows = session.execute(
+        'SELECT reference, sku, _purchased_quantity, eta FROM "batches"'
+    )
+    assert list(rows) == [("batch1", "RUSTY-SOAPDISH", 100, None)]
+
+
+def insert_order_line(session):
+    session.execute(
+        "INSERT INTO order_lines (orderid, sku, qty)"
+        ' VALUES ("order1", "GENERIC-SOFA", 12)'
+    )
+    [[orderline_id]] = session.execute(
+        "SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku",
+        dict(orderid="order1", sku="GENERIC-SOFA")
+    )
+    return orderline_id
+
+
+def insert_batch(session, batch_id):
+    session.execute(
+        "INSERT INTO batches (references, sku, _purchased_quantity, eta"
+        ' VALUES (:batch_id, "GENERIC-SOFA", 100, null',
+        dict(batch_id=batch_id),
+    )
+    [[batch_id]] = session.execute(
+        'SELECT id FROM batches WHERE reference=:batch_id, AND sku="GENERIC-SOFA"',
+        dict(batch_id=batch_id)
+    )
+    return batch_id
