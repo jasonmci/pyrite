3166cda1bc340b7c526b5065b7323cb49eb0b4e8
Jason McInerney <jason.j.mcn@gmail.com>
2023-02-18 00:25:00 -0800

    Fix lint

--- cloc_stats.sh
+++ cloc_stats.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+cloc ./ --json > cloc.json
 
-nFiles=`cloc ./**/*.py --json | jq -r ".Python.nFiles"`
-blank=`cloc ./**/*.py --json | jq -r ".Python.blank"`
-comment=`cloc ./**/*.py --json | jq -r ".Python.comment"`
-code=`cloc ./**/*.py --json | jq -r ".Python.code"`
+nFiles=`cat cloc.json | jq -r ".Python.nFiles"`
+blank=`cat cloc.json | jq -r ".Python.blank"`
+comment=`cat cloc.json | jq -r ".Python.comment"`
+code=`cat cloc.json | jq -r ".Python.code"`
 
 echo "python.code:${code}|g" | nc -w 1 -u ec2-35-90-12-71.us-west-2.compute.amazonaws.com 8125
 echo "python.comment:${comment}|g" | nc -w 1 -u ec2-35-90-12-71.us-west-2.compute.amazonaws.com 8125
