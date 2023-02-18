0d60f38558127e51b1e480e76249e658e694aedc
Jason McInerney <jason.j.mcn@gmail.com>
2023-02-17 23:42:08 -0800

    Update lines of code stats

--- cloc_stats.sh
+++ cloc_stats.sh
@@ -5,7 +5,7 @@ blank=`cloc ./**/*.py --json | jq -r ".Python.blank"`
 comment=`cloc ./**/*.py --json | jq -r ".Python.comment"`
 code=`cloc ./**/*.py --json | jq -r ".Python.code"`
 
-echo "python.code:${code}|g" | nc -w 1 -u 127.0.0.1 8125
-echo "python.comment:${comment}|g" | nc -w 1 -u 127.0.0.1 8125
-echo "python.blank:${blank}|g" | nc -w 1 -u 127.0.0.1 8125
-echo "python.files:${nFiles}|g" | nc -w 1 -u 127.0.0.1 8125
+echo "python.code:${code}|g" | nc -w 1 -u ec2-35-90-12-71.us-west-2.compute.amazonaws.com 8125
+echo "python.comment:${comment}|g" | nc -w 1 -u ec2-35-90-12-71.us-west-2.compute.amazonaws.com 8125
+echo "python.blank:${blank}|g" | nc -w 1 -u ec2-35-90-12-71.us-west-2.compute.amazonaws.com 8125
+echo "python.files:${nFiles}|g" | nc -w 1 -u ec2-35-90-12-71.us-west-2.compute.amazonaws.com 8125
